ef18da7f3f6b66bff3055e0045799559809cf4fa
Thijs Schreijer <thijs@thijsschreijer.nl>
2023-01-23 15:51:17 +0100

    fix(validator) schemas now support references (recursive as well) (#10)

--- convert/oas3.go
+++ convert/oas3.go
@@ -352,15 +352,82 @@ func generateParameterSchema(operation *openapi3.Operation) *[]map[string]interf
 			paramConf["in"] = paramValue.In
 			paramConf["name"] = paramValue.Name
 			paramConf["required"] = paramValue.Required
-			schema, _ := paramValue.Schema.Value.MarshalJSON()
-			paramConf["schema"] = string(schema)
 			paramConf["style"] = getDefaultParamStyle(paramValue.Style, paramValue.In)
+
+			schema := extractSchema(paramValue.Schema)
+			if schema != "" {
+				paramConf["schema"] = schema
+			}
+
 			result[i] = paramConf
 			i++
 		}
 	}
 
 	return &result
+}
+
+// dereferenceSchema walks the schema and adds every subschema to the seenBefore map
+func dereferenceSchema(sr *openapi3.SchemaRef, seenBefore map[string]*openapi3.Schema) {
+	if sr == nil {
+		return
+	}
+
+	if sr.Ref != "" {
+		if seenBefore[sr.Ref] != nil {
+			return
+		}
+		seenBefore[sr.Ref] = sr.Value
+	}
+
+	s := sr.Value
+
+	for _, list := range []openapi3.SchemaRefs{s.AllOf, s.AnyOf, s.OneOf} {
+		for _, s2 := range list {
+			dereferenceSchema(s2, seenBefore)
+		}
+	}
+	for _, s2 := range s.Properties {
+		dereferenceSchema(s2, seenBefore)
+	}
+	for _, ref := range []*openapi3.SchemaRef{s.Not, s.AdditionalProperties, s.Items} {
+		dereferenceSchema(ref, seenBefore)
+	}
+}
+
+// extractSchema will extract a schema, including all sub-schemas/references and
+// return it as a single JSONschema string
+func extractSchema(s *openapi3.SchemaRef) string {
+	if s == nil || s.Value == nil {
+		return ""
+	}
+
+	seenBefore := make(map[string]*openapi3.Schema)
+	dereferenceSchema(s, seenBefore)
+
+	var finalSchema map[string]interface{}
+	// copy the primary schema
+	jConf, _ := s.MarshalJSON()
+	json.Unmarshal(jConf, &finalSchema)
+
+	// inject subschema's referenced
+	if len(seenBefore) > 0 {
+		definitions := make(map[string]interface{})
+		for key, schema := range seenBefore {
+			// copy the subschema
+			var copySchema map[string]interface{}
+			jConf, _ := schema.MarshalJSON()
+			json.Unmarshal(jConf, &copySchema)
+
+			// store under new key
+			definitions[strings.Replace(key, "#/components/schemas/", "", 1)] = copySchema
+		}
+		finalSchema["definitions"] = definitions
+	}
+
+	result, _ := json.Marshal(finalSchema)
+	// update the $ref values; this is safe because plain " (double-quotes) would be escaped if in actual values
+	return strings.ReplaceAll(string(result), "\"$ref\":\"#/components/schemas/", "\"$ref\":\"#/definitions/")
 }
 
 // generateBodySchema returns the given schema if there is one, a generated
@@ -384,16 +451,7 @@ func generateBodySchema(operation *openapi3.Operation) string {
 
 	for contentType, content := range content {
 		if strings.Contains(strings.ToLower(contentType), "application/json") {
-			schema := (*content).Schema
-			if schema == nil {
-				return ""
-			}
-			schemaValue := schema.Value
-			if schemaValue == nil {
-				return ""
-			}
-			jsonSchema, _ := schemaValue.MarshalJSON()
-			return string(jsonSchema)
+			return extractSchema((*content).Schema)
 		}
 	}
 
